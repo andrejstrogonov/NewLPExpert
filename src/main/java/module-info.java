d7fb1235e7795fb6b91733b29d4adbe1509e7fee
andreistrogonov <strogonovandrej@hotmail.com>
2023-01-20 15:57:10 +0300

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.vsu.newlpexpert {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.vsu.newlpexpert to javafx.fxml;
+    exports com.vsu.newlpexpert;
+}
